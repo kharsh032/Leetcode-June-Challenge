758b6dd1ec48fc2f81387618ed599a73550e7946
Harsh <kharsh032@gmail.com>
2020-06-16 16:57:59 +0530

    Validate IP Address

---
+++ Week-3/Day-15-Search in a Binary Search Tree.cpp
@@ -0,0 +1,43 @@
+Problem  Link :- https://leetcode.com/explore/featured/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3361/
+
+/*
+
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+
+*/
+
+//Solution
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+       
+        if(root==NULL)
+              return NULL;
+        if(root->val>val)
+             return searchBST(root->left,val);
+         if(root->val<val)
+             return searchBST(root->right,val);
+        return root;
+        ///return NULL;
+    }
+};
